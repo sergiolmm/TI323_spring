b50194eb81c80f8d31d73559004166e6bbd8f13e
Sergio LMM <sergio@unicamp.br>
2026-03-16 15:12:51 -0300

    primeira versao 1.b

--- src/main/java/br/com/slmm/aula1603/OlaController.java
+++ src/main/java/br/com/slmm/aula1603/OlaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class OlaController {
 
     // tela na raiz
-    @GetMapping("/")
+    @GetMapping("/ola")
     public String home(){
 	return "Alo Mundo de TI...";
     }
